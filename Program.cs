11c72d0926204e0dd098bcf1a11de328852b66ed
owl4484 <rodeo23267286@gmail.com>
2025-05-16 19:59:02 +0900

    [Refactor] senkey to sendinput

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿// 트레이 아이콘 기반 키 자동 입력 프로그램 (WinForms 기반, 수동 트리거 & 일시정지 기능 추가)
+﻿// 트레이 아이콘 기반 키 자동 입력 프로그램 (WinForms 기반, SendInput 직접 호출 + 수동 토글 기능)
 
 using System;
 using System.Windows.Forms;
 using System.Timers;
 using System.Runtime.InteropServices;
 using System.Drawing;
-using WindowsInput;
-using WindowsInput.Native;
 
 class Program
 {
@@ -41,11 +39,11 @@ class Program
 
         // 타이머 설정
         System.Timers.Timer timer4 = new System.Timers.Timer(1 * 60 * 1000); // 1분
-        timer4.Elapsed += (s, e) => { if (isRunning) SendKey(VirtualKeyCode.VK_4, 8); };
+        timer4.Elapsed += (s, e) => { if (isRunning) SendKey(0x34, 8); }; // '4' 키
         timer4.Start();
 
         System.Timers.Timer timer30 = new System.Timers.Timer(25 * 60 * 1000); // 25분
-        timer30.Elapsed += (s, e) => { if (isRunning) SendKey(VirtualKeyCode.VK_8, 2); };
+        timer30.Elapsed += (s, e) => { if (isRunning) SendKey(0x38, 2); }; // '8' 키
         timer30.Start();
 
         Application.Run();
@@ -56,12 +54,62 @@ class Program
         timer30.Stop();
     }
 
-    static void SendKey(VirtualKeyCode key, int repeat)
+    [StructLayout(LayoutKind.Sequential)]
+    struct INPUT
     {
-        var sim = new InputSimulator();
+        public uint type;
+        public InputUnion u;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct InputUnion
+    {
+        [FieldOffset(0)] public KEYBDINPUT ki;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    const int INPUT_KEYBOARD = 1;
+    const uint KEYEVENTF_KEYUP = 0x0002;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    static void SendKey(ushort keyCode, int repeat)
+    {
         for (int i = 0; i < repeat; i++)
         {
-            sim.Keyboard.KeyPress(key);
+            INPUT[] inputs = new INPUT[2];
+
+            inputs[0].type = INPUT_KEYBOARD;
+            inputs[0].u.ki = new KEYBDINPUT
+            {
+                wVk = keyCode,
+                wScan = 0,
+                dwFlags = 0,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+
+            inputs[1].type = INPUT_KEYBOARD;
+            inputs[1].u.ki = new KEYBDINPUT
+            {
+                wVk = keyCode,
+                wScan = 0,
+                dwFlags = KEYEVENTF_KEYUP,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
             System.Threading.Thread.Sleep(200);
         }
     }
